18ecbeea2e4883faa19c1eb0eff78df065ccac16
David Lynn <david.james.lynn@gmail.com>
2019-10-26 14:41:33 +0100

    Add an initial soloution for Kata 06.
    Still getting some funny counts compared to the challenge.
    Possibly to do with encoding? Maybe a duff comparision.

---
+++ kata06/src/main/java/com/davidjlynn/codekata/kata06/AnagramFinder.java
@@ -0,0 +1,75 @@
+package com.davidjlynn.codekata.kata06;
+
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AnagramFinder {
+
+    public List<String> loadWordList() throws URISyntaxException, IOException {
+        URL wordlistUrl = Resources.getResource("wordlist.txt");
+        Path path = Paths.get(wordlistUrl.toURI());
+
+        Stream<String> lines = Files.lines(path, StandardCharsets.ISO_8859_1);
+        List<String> strings = lines.collect(Collectors.toList());
+        lines.close();
+
+        return strings;
+    }
+
+    public Map<String, List<String>> buildIntoMap(List<String> words) {
+        return words.stream()
+                .collect(Collectors.groupingBy(word -> {
+                    String lower = word.toLowerCase();
+                    char[] sortedChar = lower.toCharArray();
+                    Arrays.sort(sortedChar);
+                    String sortedString = new String(sortedChar);
+                    return sortedString;
+                }));
+    }
+
+    public List<List<String>> findAnagrams() throws URISyntaxException, IOException {
+        List<String> wordList = loadWordList();
+
+        // Sanitize *eyerole* -> Remove capitals.
+        wordList = wordList.stream().map(String::toLowerCase).distinct().collect(Collectors.toList());
+
+        Map<String, List<String>> wordMap = buildIntoMap(wordList);
+
+        // Strip out occurences of only 1 word.
+        List<List<String>> anagramLists = wordMap.values().stream()
+                .filter(anagramSet -> anagramSet.size() > 1)
+                .collect(Collectors.toList());
+
+        return anagramLists;
+    }
+
+    public void findMostAnagrams() throws URISyntaxException, IOException {
+        List<List<String>> anagramLists = findAnagrams();
+
+        List<String> anagrams = anagramLists.stream().max(Comparator.comparingInt(List::size)).get();
+
+        System.out.println(anagrams.size());
+        System.out.println(anagrams.toString());
+    }
+
+
+    public void findLongestAnagrams() throws URISyntaxException, IOException {
+        List<List<String>> anagramLists = findAnagrams();
+
+        List<String> anagrams = anagramLists.stream().max(Comparator.comparingInt((anagramList -> anagramList.get(0).length()))).get();
+
+        System.out.println(anagrams.size());
+        System.out.println(anagrams.toString());
+    }
+
+}
---
+++ kata06/src/test/java/com/davidjlynn/codekata/kata06/AnagramFinderTest.java
@@ -0,0 +1,60 @@
+package com.davidjlynn.codekata.kata06;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
+public class AnagramFinderTest {
+
+    private AnagramFinder anagramFinder = new AnagramFinder();
+
+    @Test
+    public void loadWordList_ok() throws IOException, URISyntaxException {
+        // RUN TEST
+        List<String> strings = anagramFinder.loadWordList();
+
+        // CHECK RESULTS
+        Assert.assertNotNull(strings);
+        Assert.assertEquals(338882, strings.size());
+    }
+
+    @Test
+    public void buildIntoMap_ok() throws IOException, URISyntaxException {
+        // RUN TEST
+        List<String> strings = anagramFinder.loadWordList();
+
+        Map<String, List<String>> stringListMap = anagramFinder.buildIntoMap(strings);
+
+        // CHECK RESULTS
+        Assert.assertNotNull(stringListMap);
+        Assert.assertNotNull(stringListMap.keySet());
+        Assert.assertEquals(0, stringListMap.keySet().size());
+    }
+
+    @Test
+    public void findAnagrams_ok() throws IOException, URISyntaxException {
+        // RUN TEST
+        List<List<String>> anagramLists = anagramFinder.findAnagrams();
+
+        // CHECK RESULTS
+        Assert.assertNotNull(anagramLists);
+        Assert.assertEquals(0, anagramLists.size());
+    }
+
+    @Test
+    public void findMostAnagrams_ok() throws IOException, URISyntaxException {
+        // RUN TEST
+        anagramFinder.findMostAnagrams();
+    }
+
+    @Test
+    public void findLongestAnagrams_ok() throws IOException, URISyntaxException {
+        // RUN TEST
+        anagramFinder.findLongestAnagrams();
+    }
+
+}
