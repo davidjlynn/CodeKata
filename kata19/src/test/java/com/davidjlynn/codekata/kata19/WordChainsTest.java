589840f9cb07464a08fc45c40c7978e02cd90026
David Lynn <david.james.lynn@gmail.com>
2020-02-24 23:18:19 +0000

    Add Timing for kata 19

--- kata19/src/test/java/com/davidjlynn/codekata/kata19/WordChainsTest.java
+++ kata19/src/test/java/com/davidjlynn/codekata/kata19/WordChainsTest.java
@@ -2,6 +2,8 @@ package com.davidjlynn.codekata.kata19;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,9 +12,14 @@ public class WordChainsTest {
   @Test
   public void test1() throws IOException, URISyntaxException {
     WordChains wordChains = new WordChains();
+
+    Instant start = Instant.now();
+
     List<String> words = wordChains.findWordChain("cat", "dog");
+
+    Instant end = Instant.now();
+    System.out.println("Time taken to run word chain: " + Duration.between(start, end));
 
     Assert.assertEquals(4, words.size());
   }
-
 }
